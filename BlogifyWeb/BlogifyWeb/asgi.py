9be37b0fe159627fbe5c33cda4b79a417146273f
omeravner2 <omeravner2@gmail.com>
2023-09-21 13:33:55 +0300

    Initial Commit

---
+++ BlogifyWeb/BlogifyWeb/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for BlogifyWeb project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlogifyWeb.settings')
+
+application = get_asgi_application()
